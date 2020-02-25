3c65294588696e9bd007c006cc3734f444745eec
yerowyn <yerowyn@gmail.com>
2020-02-25 15:22:54 +0600

    add navbar to search page

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import NavBar from '../NavBar/NavBar.js';
+import SearchBar from '../SearchBar/SearchBar.js';
 
 function Search(props) {
 	return (
 		<div>
-			Search
+			<NavBar />
+			<SearchBar />
+			<button className={`button ${styles['nav-button']}`}>Log In</button>
+			<button className={`button ${styles['nav-button']}`}>Register</button>
 		</div>
 	);
 }
