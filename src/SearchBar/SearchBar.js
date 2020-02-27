1fa572da2c48457fa97866b0de0078c9fa377290
yerowyn <yerowyn@gmail.com>
2020-02-27 15:19:16 +0600

    add term and location redirect

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import styles from './SearchBar.module.css';
 
+
 function SearchBar(props) {
+	const [term, setTerm] = useState(props.term || '');
+	const [location, setLocation] = useState(props.location || '');
 	const sizeClass = props.small ? '': 'is-medium';
+
+	function submit(e) {
+		if(typeof props.search === 'function'){
+			props.search(term, location);
+		}
+		console.log(term, location);
+		e.preventDefault();
+	}
   return (
-		<form action="#">
+		<form onSubmit={submit}>
 			<div className="field has-addons">
 				<p className="control">
 					<button className={`button is-static ${sizeClass}`}>Search</button>
@@ -14,19 +25,23 @@ function SearchBar(props) {
 						className={`input ${sizeClass} ${styles['input-control']}`}
 						type="text"
 						placeholder="burgers, barbers, spas, handyman"
+						value={term}
+						onChange={(e) => setTerm(e.target.value)}
 					/>
 				</p>
 				<p className="control">
-					<button className={`button is-static  ${sizeClass}`}>NEAR</button>
+					<div className={`button is-static  ${sizeClass}`}>NEAR</div>
 				</p>
 				<p className="control">
 					<input
 						className={`input ${sizeClass} ${styles['input-control']}`}
 						type="text"
 						placeholder="Where"
+						value={location}
+						onChange={(e) => setLocation(e.target.value)}
 					/>
 				</p>
-				<div className={`button ${styles['search-button']} ${sizeClass}`}>
+				<div className={`button ${styles['search-button']} ${sizeClass}`} onClick={submit}>
 					<span className={`icon is-small ${styles['search-icon']}`}>
 						<i className="fas fa-search"></i>
 					</span>
