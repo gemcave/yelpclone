652990ed083d9fc13767ff04dfe70e1e311c8b4d
yerowyn <yerowyn@gmail.com>
2020-02-27 13:49:36 +0600

    add search suggestions

--- src/SearchSuggestions/SearchSuggestions.js
+++ src/SearchSuggestions/SearchSuggestions.js
@@ -4,22 +4,10 @@ import styles from './SearchSuggestions.module.css';
 function SearchSuggestions(props) {
   return (
     <div className={styles.suggestions}>
-      <span className="icon is-small">
-        <i className="fas fa-utensils"></i>
-      </span>
-      <span className={styles.suggestion}>Restaurants</span>
-      <span className="icon is-small">
-        <i className="fas fa-cocktail"></i>
-      </span>
-      <span className={styles.suggestion}>Nightlife</span>
-      <span className="icon is-small">
-        <i className="fas fa-concierge-bell"></i>
-      </span>
-      <span className={styles.suggestion}>Services</span>
-      <span className="icon is-small">
-        <i className="fas fa-truck"></i>
-      </span>
-      <span className={styles.suggestion}>Delivary Services</span>
+      <span className="icon is-small"><i className="fas fa-utensils"></i></span><span className={styles.suggestion}>Restaurants</span>
+      <span className="icon is-small"><i className="fas fa-cocktail"></i></span><span className={styles.suggestion}>Nightlife</span>
+      <span className="icon is-small"><i className="fas fa-concierge-bell"></i></span><span className={styles.suggestion}>Services</span>
+      <span className="icon is-small"><i className="fas fa-truck"></i></span><span className={styles.suggestion}>Delivary Services</span>
     </div>
   );
 }
