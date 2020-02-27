1f8f4fbffcdc0bd051613bd63dd96f0c5c9fda56
yerowyn <yerowyn@gmail.com>
2020-02-27 13:49:18 +0600

    add search filters and info

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -2,12 +2,15 @@ import React from 'react';
 import NavBar from '../NavBar/NavBar.js';
 import SearchBar from '../SearchBar/SearchBar.js';
 import styles from './Search.module.css';
+import SubNav from '../NavBar/SubNav/SubNav.js';
+import SearchResultSummary from './SearchResultSummary/SearchResultSummary.js';
 
 function Search(props) {
 	return (
 		<div>
 			<NavBar />
-			{/* <SearchBar /> */}
+			<SubNav />
+			<SearchResultSummary />
 		</div>
 	);
 }
